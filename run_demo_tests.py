aafbf11d29c6e7a8cbbdbc4f8a454c69169bf476
Konrad Kurdej <konrad.kurdej@10clouds.com>
2013-07-11 14:53:45 +0200

    Adjusting URL from command line

--- run_demo_tests.py
+++ run_demo_tests.py
@@ -1,10 +1,18 @@
 import unittest
 import xmlrunner
+import sys
 
 from xvfbwrapper import Xvfb
 
+import settings
+
 vdisplay = Xvfb(width=1280, height=720)
 vdisplay.start()
+
+if len(sys.argv) > 1:
+    settings.Settings.url = sys.argv[1]
+    sys.argv = sys.argv[:1]
+
 print "Started Xvfb"
 
 unittest.main(testRunner=xmlrunner.XMLTestRunner(output='selenium-demos-reports', verbosity=2),
